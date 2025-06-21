1cbf5782016155e3ab048b8d0f89c30f49280699
bradleypregon <bradley.pregon@gmail.com>
2025-06-21 15:00:56 -0500

    Small tweaks, port number change

--- src/api.py
+++ src/api.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
-import efbUI
+import SBCleaner
 app = FastAPI()
 
 @app.get("/")
 def getBlank():
   return {"Hello": "World"}
 
-@app.get("/latest/{simbriefID}")
-def fetchRoute(simbriefID):
-  return efbUI.main(simbriefID)
+@app.get("/latest/{sbID}")
+def fetchRoute(sbID):
+  return efbUI.main(sbID)
 
 if __name__ == "__main__":
-  config = uvicorn.Config(app, port=8005, host="0.0.0.0", proxy_headers=True)
+  config = uvicorn.Config(app, port=65010, host="0.0.0.0")
   server = uvicorn.Server(config)
   server.run()
